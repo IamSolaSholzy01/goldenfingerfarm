726c043e176d3808b7d5c3dcb3bdac798866514d
IamSolaSholzy01 <ogolagunju@gmail.com>
2022-09-01 07:26:29 +0100

    Project Init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from "../theme/GlobalStyle";
+import {AppTheme} from "../theme/AppTheme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <ThemeProvider theme={AppTheme}>
+        <GlobalStyle hasNoFocus={true}/><Component {...pageProps} />
+      </ThemeProvider>
+  )
 }
 
 export default MyApp
