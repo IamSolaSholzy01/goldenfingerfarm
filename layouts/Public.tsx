493ca2aea46bc81042f8ec57a8d24260a0edc055
IamSolaSholzy01 <ogolagunju@gmail.com>
2022-09-05 15:22:02 +0100

    Home Section

--- layouts/Public.tsx
+++ layouts/Public.tsx
@@ -1,41 +1,48 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
 import styled from "styled-components";
-import {primaryColors} from "../theme/colors";
+import { primaryColors } from "../theme/colors";
 import useMediaQuery from "../hooks/useMediaQuery";
-import logoPic from "../public/images/logo.png"
+import logoPic from "../public/images/logo.png";
 // import {CartIcon} from "../components/icons/CartIcon";
-import React, {ReactNode, useEffect} from "react";
-import {useRouter} from "next/router";
+import React, { ReactNode, useEffect } from "react";
+import { useRouter } from "next/router";
 
 const links = [
-    {label: "Home", href: "/"},
-    {label: "About us", href: "/about"},
-    {label: "Orders", href: "/orders"},
-    {label: "Academy", href: "/academy"},
-    {label: "Contact us", href: "/contact"},
-]
+  { label: "Home", href: "/" },
+  { label: "About us", href: "/about" },
+  { label: "Orders", href: "/orders" },
+  { label: "Academy", href: "/academy" },
+  { label: "Contact us", href: "/contact" },
+];
 
 const footerLinks = [
-    {label: "Help and Ordering", href: "/help"},
-    {label: "Privacy Policy", href: "/privacy"},
-    {label: "Return and Cancellation", href: "/return"},
-    {label: "Delivery Schedule", href: "/schedule"},
-    {label: "Get a Call", href: "/contact"},
-    {label: "Online Enquiry", href: "/enquiry"},
-]
+  { label: "Help and Ordering", href: "/help" },
+  { label: "Privacy Policy", href: "/privacy" },
+  { label: "Return and Cancellation", href: "/return" },
+  { label: "Delivery Schedule", href: "/schedule" },
+  { label: "Get a Call", href: "/contact" },
+  { label: "Online Enquiry", href: "/enquiry" },
+];
 
-let mobileLinks = [...links]
-mobileLinks.push({label: 'Login', href: "/login"})
-mobileLinks.push({label: 'Sign up', href: "/signup"})
+let mobileLinks = [...links];
+mobileLinks.push({ label: "Login", href: "/login" });
+mobileLinks.push({ label: "Sign up", href: "/signup" });
 
-const Burg: ({open, className}: { open: boolean, className?: string }) => JSX.Element = ({className}) => <div
-    className={className}>
+const Burg: ({
+  open,
+  className,
+}: {
+  open: boolean;
+  className?: string;
+}) => JSX.Element = ({ className }) => (
+  <div className={className}>
     <div></div>
     <div></div>
     <div></div>
-</div>
+  </div>
+);
 const Burger = styled(Burg)`
   position: relative;
   width: 35px;
@@ -43,42 +50,53 @@ const Burger = styled(Burg)`
   & > div {
     position: absolute;
     height: 2px;
-    width: ${({open}) => open ? 0 : '35px'};
-    background-color: ${({open, theme}) => !open ? theme.colors.grayScale.darkGrey : theme.colors.grayScale.white};;
+    width: ${({ open }) => (open ? 0 : "35px")};
+    background-color: ${({ open, theme }) =>
+      !open ? theme.colors.grayScale.darkGrey : theme.colors.grayScale.white};
     transition: transform 0.5s ease;
 
     &:first-of-type {
-      transform: ${({open}) => open ? 'rotate(45deg)' : 'translateY(-10px)'};
+      transform: ${({ open }) =>
+        open ? "rotate(45deg)" : "translateY(-10px)"};
       width: 35px;
     }
 
     &:last-of-type {
-      transform: ${({open}) => open ? 'rotate(315deg)' : 'translateY(10px)'};
+      transform: ${({ open }) =>
+        open ? "rotate(315deg)" : "translateY(10px)"};
       width: 35px;
     }
   }
+`;
 
-`
-
-const MNav: ({className, open}: { className?: string, open?: boolean }) => JSX.Element = ({className}) => <div
-    className={className}>
-    <ul>{
-        mobileLinks.map(({label, href}) => <li key={label}>
-            <Link href={href}>{label}</Link>
-        </li>)
-    }</ul>
-</div>
+const MNav: ({
+  className,
+  open,
+}: {
+  className?: string;
+  open?: boolean;
+}) => JSX.Element = ({ className }) => (
+  <div className={className}>
+    <ul>
+      {mobileLinks.map(({ label, href }) => (
+        <li key={label}>
+          <Link href={href}>{label}</Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 const MobileNav = styled(MNav)`
-  display: ${({open}) => open ? 'block' : 'none'};
+  display: ${({ open }) => (open ? "block" : "none")};
   position: fixed;
   height: 100%;
-  width: ${({open}) => open ? '100%' : 0};
+  width: ${({ open }) => (open ? "100%" : 0)};
   top: 0;
   right: 0;
-  background-image: url('/images/bgnav.png');
+  background-image: url("/images/bgnav.png");
   background-size: contain;
-  background-color: ${({theme}) => theme.colors.primary.green};
+  background-color: ${({ theme }) => theme.colors.primary.green};
   transition: width 2s ease;
   z-index: 1;
 
@@ -94,52 +112,68 @@ const MobileNav = styled(MNav)`
       border-bottom: 1px solid #fff;
     }
   }
-`
+`;
 
 const BurgerContainer = styled.div<{ open?: boolean }>`
   z-index: 2;
-  margin-top: ${({open}) => open ? '4rem' : 0};
-  margin-bottom: ${({open}) => open ? '-4rem' : 0};
-
-`
+  margin-top: ${({ open }) => (open ? "4rem" : 0)};
+  margin-bottom: ${({ open }) => (open ? "-4rem" : 0)};
+`;
 
 const Heading: ({
-                    isMobile,
-                    className,
-                    title
-                }: { isMobile: boolean, className?: string, title?: string }) => JSX.Element = ({
-                                                                                                    isMobile,
-                                                                                                    className,
-                                                                                                    title
-                                                                                                }) => {
-    const [burgerOpen, setBurgerOpen] = React.useState(false);
-    useEffect(() => {
-        setBurgerOpen(false)
-    }, [title]);
-    const router = useRouter();
-    return (
-        <div className={className}>
-            {isMobile ? <header className={'mobileHeader'}>
-                <div className="logoWrapper"><Image src={logoPic} alt={'logo'}/></div>
-                <BurgerContainer open={burgerOpen} onClick={() => setBurgerOpen(!burgerOpen)}><Burger
-                    open={burgerOpen}/></BurgerContainer>
-            </header> : <header className={'desktopHeader'}>
-                <div className="logoWrapper"><Image src={logoPic} alt={'logo'}/></div>
-                <ul>{
-                    links.map(({label, href}) => <li key={label} className={router.pathname == href ? "active" : ""}>
-                        <Link href={href}>{label}</Link>
-                    </li>)
-                }</ul>
-                {/*<Link href={'/checkout'}><div className={'cartContainer'}><CartIcon/></div></Link>*/}
-                <div className={'auth'}>
-                    <Link href={'/login'}>Login</Link>
-                    <Link href={'/signup'}>Sign up</Link>
-                </div>
-            </header>}
-            {isMobile && <MobileNav open={burgerOpen}/>}
-        </div>
-    )
-}
+  isMobile,
+  className,
+  title,
+}: {
+  isMobile: boolean;
+  className?: string;
+  title?: string;
+}) => JSX.Element = ({ isMobile, className, title }) => {
+  const [burgerOpen, setBurgerOpen] = React.useState(false);
+  useEffect(() => {
+    setBurgerOpen(false);
+  }, [title]);
+  const router = useRouter();
+  return (
+    <div className={className}>
+      {isMobile ? (
+        <header className={"mobileHeader"}>
+          <div className="logoWrapper">
+            <Image src={logoPic} alt={"logo"} />
+          </div>
+          <BurgerContainer
+            open={burgerOpen}
+            onClick={() => setBurgerOpen(!burgerOpen)}
+          >
+            <Burger open={burgerOpen} />
+          </BurgerContainer>
+        </header>
+      ) : (
+        <header className={"desktopHeader"}>
+          <div className="logoWrapper">
+            <Image src={logoPic} alt={"logo"} />
+          </div>
+          <ul>
+            {links.map(({ label, href }) => (
+              <li
+                key={label}
+                className={router.pathname == href ? "active" : ""}
+              >
+                <Link href={href}>{label}</Link>
+              </li>
+            ))}
+          </ul>
+          {/*<Link href={'/checkout'}><div className={'cartContainer'}><CartIcon/></div></Link>*/}
+          <div className={"auth"}>
+            <Link href={"/login"}>Login</Link>
+            <Link href={"/signup"}>Sign up</Link>
+          </div>
+        </header>
+      )}
+      {isMobile && <MobileNav open={burgerOpen} />}
+    </div>
+  );
+};
 
 const Header = styled(Heading)`
   header {
@@ -167,23 +201,24 @@ const Header = styled(Heading)`
 
         a::after {
           width: 0;
-          content: '';
+          content: "";
           position: absolute;
           bottom: -40%;
           height: 3px;
           left: 0;
           transition: all 0.3s ease-in-out;
-          background-color: ${({theme}) => theme.colors.primary.green};
+          background-color: ${({ theme }) => theme.colors.primary.green};
         }
 
-        a:hover::after, &.active a::after {
+        a:hover::after,
+        &.active a::after {
           content: "";
           width: 100%;
         }
       }
 
       li.active {
-        color: ${({theme}) => theme.colors.primary.green};
+        color: ${({ theme }) => theme.colors.primary.green};
       }
     }
 
@@ -218,62 +253,79 @@ const Header = styled(Heading)`
     }
 
     a:first-of-type {
-      border: 1px solid ${({theme}) => theme.colors.primary.green};
+      border: 1px solid ${({ theme }) => theme.colors.primary.green};
     }
 
     a:last-of-type {
-      background-color: ${({theme}) => theme.colors.primary.green};
-      color: ${({theme}) => theme.colors.grayScale.white}
+      background-color: ${({ theme }) => theme.colors.primary.green};
+      color: ${({ theme }) => theme.colors.grayScale.white};
     }
   }
-`
+`;
 
-const Footing: ({isMobile, className}: { isMobile: boolean, className?: string }) => JSX.Element = ({
-                                                                                                        className
-                                                                                                    }) => <div
-    className={className}>
+const Footing: ({
+  isMobile,
+  className,
+}: {
+  isMobile: boolean;
+  className?: string;
+}) => JSX.Element = ({ className }) => (
+  <div className={className}>
     <div>
-        <div className={"socialSection"}>
-            <div className="logoWrapper"><Image src={logoPic} alt={'logo'}/></div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum cras vel donec maecenas eu.Lorem ipsum
-                dolor sit amet, consectetur adipiscing elit. Ipsum </p>
+      <div className={"socialSection"}>
+        <div className="logoWrapper">
+          <Image src={logoPic} alt={"logo"} />
         </div>
-        <div className={'supportSection'}>
-            <h3>Customer Support</h3>
-            <ul>
-                {footerLinks.map(({label, href}, index) => <li key={index}><Link href={href}>{label}</Link></li>)}
-            </ul>
-        </div>
-        <div className={'newsletterSection'}>
-            <h3>NewsLetter</h3>
-            <form>
-                <div>
-                    <label>Enter your email and we'll send you the latest plans</label>
-                    <input type={'text'} placeholder={'Enter your email'} required/>
-                </div>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum cras
+          vel donec maecenas eu.Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Ipsum{" "}
+        </p>
+      </div>
+      <div className={"supportSection"}>
+        <h3>Customer Support</h3>
+        <ul>
+          {footerLinks.map(({ label, href }, index) => (
+            <li key={index}>
+              <Link href={href}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={"newsletterSection"}>
+        <h3>NewsLetter</h3>
+        <form>
+          <div>
+            <label>Enter your email and we'll send you the latest plans</label>
+            <input type={"text"} placeholder={"Enter your email"} required />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
-    <span>Copyright &copy; 2022. <em>Goldenfinger Farm and Ranches Ltd.</em> All Right Reserved.</span>
-</div>
+    <span>
+      Copyright &copy; 2022. <em>Goldenfinger Farm and Ranches Ltd.</em> All
+      Right Reserved.
+    </span>
+  </div>
+);
 
 const Footer = styled(Footing)`
   & > div {
     display: flex;
-    flex-direction: ${({isMobile}) => isMobile ? 'column' : 'row'};
-    align-items: ${({isMobile}) => isMobile ? 'center' : 'flex-start'};;
-    padding: ${({isMobile}) => isMobile ? '20px' : '90px'};
-    text-align: ${({isMobile}) => isMobile ? 'center' : 'left'};
+    flex-direction: ${({ isMobile }) => (isMobile ? "column" : "row")};
+    align-items: ${({ isMobile }) => (isMobile ? "center" : "flex-start")};
+    padding: ${({ isMobile }) => (isMobile ? "20px" : "90px")};
+    text-align: ${({ isMobile }) => (isMobile ? "center" : "left")};
 
     & > div {
       padding: 20px 10px;
-      width: ${({isMobile}) => isMobile ? '100%' : 'calc(100%/3)'};
+      width: ${({ isMobile }) => (isMobile ? "100%" : "calc(100%/3)")};
     }
 
     .socialSection {
       padding: 0 10px;
-      text-align: ${({isMobile}) => isMobile ? 'center' : 'left'};
+      text-align: ${({ isMobile }) => (isMobile ? "center" : "left")};
 
       p {
         margin: 0 auto;
@@ -286,13 +338,15 @@ const Footer = styled(Footing)`
       }
     }
 
-    .supportSection, .newsletterSection {
+    .supportSection,
+    .newsletterSection {
       h3 {
         margin-bottom: 35px;
         font-size: 2rem;
       }
 
-      li, label {
+      li,
+      label {
         font-size: 1.5rem;
         margin-bottom: 1.375rem;
       }
@@ -301,7 +355,7 @@ const Footer = styled(Footing)`
     .newsletterSection {
       label {
         display: block;
-        padding: ${({isMobile}) => isMobile ? '0 40px' : '0'};
+        padding: ${({ isMobile }) => (isMobile ? "0 40px" : "0")};
         margin: 0 auto;
         max-width: 500px;
       }
@@ -322,8 +376,8 @@ const Footer = styled(Footing)`
         display: block;
         margin: auto;
         font-size: 2rem;
-        background-color: ${({theme}) => theme.colors.primary.yellow};
-        color: ${({theme}) => theme.colors.grayScale.white};
+        background-color: ${({ theme }) => theme.colors.primary.yellow};
+        color: ${({ theme }) => theme.colors.grayScale.white};
         border: 0;
         border-radius: 10px;
         outline: none;
@@ -339,39 +393,45 @@ const Footer = styled(Footing)`
     font-size: 1.25rem;
 
     em {
-      color: ${({theme}) => theme.colors.primary.yellow}
+      color: ${({ theme }) => theme.colors.primary.yellow};
     }
   }
-`
+`;
 const Public: ({
-                   title,
-                   children,
-                   className
-               }: { title: string, children: ReactNode, className?: string }) => JSX.Element = ({
-                                                                                                    title,
-                                                                                                    children,
-                                                                                                    className
-                                                                                                }) => {
-    const isMobile = useMediaQuery('(max-width: 895px)')
-    return <div className={className}>
-        <Head>
-            <title>{title + ' - GoldenFingerFarm'}</title>
-            <meta name="description" content="GoldenFingerFarm and Ranches Limited"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
-            <meta name="theme-color" content={primaryColors['green'] as string}/>
-            <link rel="icon" href="/favicon.ico"/>
-        </Head>
-        <Header title={title} isMobile={isMobile}/>
-        <main>
-            {children}
-        </main>
-        <footer>
-            <Footer isMobile={isMobile}/>
-        </footer>
+  title,
+  children,
+  className,
+}: {
+  title: string;
+  children: ReactNode;
+  className?: string;
+}) => JSX.Element = ({ title, children, className }) => {
+  const isMobile = useMediaQuery("(max-width: 895px)");
+  return (
+    <div className={className}>
+      <Head>
+        <title>{title + " - GoldenFingerFarm"}</title>
+        <meta
+          name="description"
+          content="GoldenFingerFarm and Ranches Limited"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+        />
+        <meta name="theme-color" content={primaryColors["green"] as string} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header title={title} isMobile={isMobile} />
+      <main>{children}</main>
+      <footer>
+        <Footer isMobile={isMobile} />
+      </footer>
     </div>
-}
+  );
+};
 
 const PublicLayout = styled(Public)`
-  background-color: ${({theme}) => theme.colors.grayScale.white};
-`
-export default PublicLayout
+  background-color: ${({ theme }) => theme.colors.grayScale.white};
+`;
+export default PublicLayout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,150 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.scss";
 import styled from "styled-components";
 import useMediaQuery from "../hooks/useMediaQuery";
-import {PrimaryButton} from "../components/buttons";
+import { PrimaryButton } from "../components/buttons";
+// import solarImage from "../public/images/home/solar.png";
+// import birdsImg from "../public/images/home/birds.jpeg";
+// import vegetablesImage from "../public/images/home/vegetables.jpeg";
+// import farmerImage from "../public/images/home/farmer.jpeg";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+
+const specialties = [
+  {
+    image: "vegetables.jpeg",
+    title: "Vegetables and Grains",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum cras vel donec maecenas eu.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum ",
+    slug: "",
+  },
+  {
+    image: "farmer.jpeg",
+    title: "Livestock and fisheries",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum cras vel donec maecenas eu.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum ",
+    slug: "",
+  },
+  {
+    image: "birds.jpeg",
+    title: "Poultry farming",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum cras vel donec maecenas eu.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum ",
+    slug: "",
+  },
+  {
+    image: "solar.png",
+    title: "Green house",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum cras vel donec maecenas eu.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum ",
+    slug: "",
+  },
+];
 
 const classJoin = (...classes: string[]) => {
-  return classes.join(" ")
-}
+  return classes.join(" ");
+};
 
-const Home: ({title, className}: { title: string, className: string }) => JSX.Element = ({className}) => {
-    const isMobile = useMediaQuery('(max-width: 895px)')
-    return (
-        <div className={className}>
-            <section className={classJoin(styles.bgSection, (isMobile ? styles.smBgSection : styles.lgBgSection))}>
-                <h1>Know your <span>farmer,</span></h1>
-                <h1>Know your <span>food.</span></h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum cras vel donec maecenas eu.Lorem
-                    ipsum dolor sit amet, consectetur adipiscing elit. Ipsum
-                </p>
-                <PrimaryButton text={'Explore Market'} />
-            </section>
+const Home: ({
+  title,
+  className,
+}: {
+  title: string;
+  className: string;
+}) => JSX.Element = ({ className }) => {
+  const router = useRouter();
+  const isMobile = useMediaQuery("(max-width: 895px)");
+  return (
+    <div className={className}>
+      <section
+        className={classJoin(
+          styles.bgSection,
+          isMobile ? styles.smBgSection : styles.lgBgSection
+        )}
+      >
+        <h1>
+          Know your <span>farmer,</span>
+        </h1>
+        <h1>
+          Know your <span>food.</span>
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum cras
+          vel donec maecenas eu.Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Ipsum
+        </p>
+        <PrimaryButton text={"Explore Market"} />
+      </section>
+      <section
+        className={classJoin(
+          styles.specialtiesSection,
+          isMobile ? styles.smSpecialtiesSection : styles.lgSpecialtiesSection
+        )}
+      >
+        <h2>What we do</h2>
+        <p>
+          We specialise in the organic production of various kinds of food crops
+          including grains, vegetables; poultry & birds - layers and boilers as
+          well as livestock including cows and bulls.
+        </p>
+        <div>
+          {specialties.map(({ title, description, image, slug }, index) => (
+            <div key={index}>
+              <div className={styles.imageContainer}>
+                <Image
+                  src={"/images/home/" + image}
+                  alt={title}
+                  layout={"fill"}
+                />
+              </div>
+              <h3>{title}</h3>
+              <p>{description}</p>
+              <PrimaryButton
+                text={"Read More"}
+                onClick={() => router.push(`/products/${slug}`)}
+              />
+            </div>
+          ))}
         </div>
-    )
-}
+        <p>
+          <Link href="">
+            High end agriculture for all (View all our activities)
+          </Link>
+        </p>
+      </section>
+    </div>
+  );
+};
 
 const HomeSection = styled(Home)`
   section:first-of-type {
     h1 {
       font-weight: 600;
-      color: ${({theme}) => theme.colors.grayScale.white};
+      color: ${({ theme }) => theme.colors.grayScale.white};
     }
 
     h1:first-of-type {
       span {
-        color: ${({theme}) => theme.colors.primary.green};
+        color: ${({ theme }) => theme.colors.primary.green};
       }
     }
 
     h1:last-of-type {
       span {
-        color: ${({theme}) => theme.colors.primary.yellow};
+        color: ${({ theme }) => theme.colors.primary.yellow};
       }
     }
-    
+
     p {
       font-size: 1.5rem;
-      color: ${({theme}) => theme.colors.grayScale.white};
+      color: ${({ theme }) => theme.colors.grayScale.white};
     }
   }
-`
+`;
 
 export async function getStaticProps() {
-    const title = 'Home'
-    return {props: {title}}
+  const title = "Home";
+  return { props: { title } };
 }
 
-export default HomeSection
+export default HomeSection;
