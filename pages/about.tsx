a74da957462e630b48ac305bbdfca774822c829f
Powerisinschool <toolulopeolagunju@gmail.com>
2022-09-05 17:41:16 +0100

    minor changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,6 +4,7 @@ import useMediaQuery from "../hooks/useMediaQuery";
 import {PrimaryButton} from "../components/buttons";
 import Image from "next/image";
 import aboutBg from "../public/images/aboutbg.webp";
+import fruits from "../public/images/fruits.webp";
 
 const classJoin = (...classes: string[]) => {
     return classes.join(" ")
@@ -23,6 +24,21 @@ const About: ({title, className}: { title: string, className: string }) => JSX.E
                 <div className={styles.herotext}>
                     <h1 className={styles.h1}>About <span className={styles.green}>Us</span></h1>
                     <span>carousel</span>
+                </div>
+            </section>
+            <section className={styles.explore}>
+                <div className={styles.row}>
+                    <div className={styles.col}>
+                        <h2 className={styles.h2}>Welcome to <h1 className={styles.h1}>Golden Finger <span className={styles.green}>Farm</span> & <span className={styles.yellow}>Ranches</span> Ltd.</h1></h2>
+                        <PrimaryButton text="Explore Market" onClick={() => {}} style={{width: ""}} />
+                    </div>
+                    <Image src={fruits} alt="Fruits" width={400} height={409} />
+                </div>
+                <div>
+                    <h2 className={styles.h2}>Our Vision</h2>
+                </div>
+                <div>
+                    <h2 className={styles.h2}>Our Mission</h2>
                 </div>
             </section>
         </div>
