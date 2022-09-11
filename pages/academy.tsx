5ed4ee2024f78806f586241f55fd6bfc323b7ca1
IamSolaSholzy01 <ogolagunju@gmail.com>
2022-09-11 20:31:45 +0100

    Couple of additions

--- pages/academy.tsx
+++ pages/academy.tsx
@@ -2,7 +2,11 @@ import styled from "styled-components";
 import styles from "../styles/Academy.module.scss";
 import useMediaQuery from "../hooks/useMediaQuery";
 import { classJoin } from "./index";
+import Flex from "../components/containers/flex/Flex";
+import { PrimaryButton } from "../components/buttons";
+import Grid from "../components/containers/grid/Grid";
 
+const lessons = [];
 const About: ({
   title,
   className,
@@ -24,6 +28,63 @@ const About: ({
           Golden Fingers Farm and Ranches{" "}
           <span className={styles.yellow}>Academy.</span>
         </h2>
+      </section>
+      <section>
+        <Flex direction={isMobile ? "column-reverse" : "row"}>
+          <div>
+            <h3>Creating wealth through farming</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum
+              cras vel donec maecenas eu.Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Ipsum{" "}
+            </p>
+            <PrimaryButton text={"Apply Now"} />
+          </div>
+          <Grid columns={4} rows={4}>
+            All the images
+          </Grid>
+        </Flex>
+      </section>
+      <section>
+        <h3>What to expect</h3>
+        {lessons.map(({ title, image, description }, index) => (
+          <Flex
+            key={index}
+            direction={isMobile ? "column" : "row"}
+            align={"center"}
+          >
+            <div>Image Container</div>
+            <div>
+              <h5>{title}</h5>
+              <p>{description}</p>
+            </div>
+          </Flex>
+        ))}
+      </section>
+      <section>
+        <h3>Product Gallery</h3>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum cras vel
+        donec maecenas eu.Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Ipsum
+        <div>
+          <h5>Pictures</h5>
+          <div>Pictures component</div>
+        </div>
+        <div>
+          <h5>Videos</h5>
+          <div>Videos component</div>
+        </div>
+      </section>
+      <section>
+        <h2>
+          Eat <span className={styles.green}>healthy,</span> live{" "}
+          <span className={styles.yellow}>healthy</span>
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum cras
+          vel donec maecenas eu.Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Ipsum{" "}
+        </p>
       </section>
     </div>
   );
---
+++ pages/orders.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import useMediaQuery from "../hooks/useMediaQuery";
+
+const Orders: ({
+  title,
+  className,
+}: {
+  title: string;
+  className: string;
+}) => JSX.Element = ({ className }) => {
+  // const isMobile = useMediaQuery("(max-width: 895px)");
+
+  return <div className={className}></div>;
+};
+
+const OrderSection = styled(Orders)``;
+export async function getStaticProps() {
+  const title = "Orders";
+  return { props: { title } };
+}
+
+export default OrderSection;
