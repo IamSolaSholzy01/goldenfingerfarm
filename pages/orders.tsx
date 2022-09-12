dc20dd3ae8b3664e6b47ea7898d364f5f885e6f7
IamSolaSholzy01 <ogolagunju@gmail.com>
2022-09-12 07:54:57 +0100

    Order section

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
-// import useMediaQuery from "../hooks/useMediaQuery";
+import styles from "../styles/Orders.module.scss";
+import Flex from "../components/containers/flex/Flex";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { PrimaryButton } from "../components/buttons";
 
 const Orders: ({
   title,
@@ -8,9 +11,38 @@ const Orders: ({
   title: string;
   className: string;
 }) => JSX.Element = ({ className }) => {
-  // const isMobile = useMediaQuery("(max-width: 895px)");
+  const isMobile = useMediaQuery("(max-width: 895px)");
 
-  return <div className={className}></div>;
+  return (
+    <div className={className}>
+      <section className={styles.mainProduct}>
+        <h2 className={styles.yellow}>Order Information</h2>
+        <Flex direction={isMobile ? "column" : "row"}>
+          <div>
+            Product Image
+            <h5>Product Name</h5>
+          </div>
+          <div>
+            <h5>Description</h5>
+            <p>Description Content</p>
+            <h4 className={styles.yellow}>Price: &#8358;10,000 /pack</h4>
+            <Flex direction={isMobile ? "column" : "row"}>
+              <Flex>
+                <span>Add quantity</span>
+                <div>
+                  <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                  </select>
+                </div>
+              </Flex>
+              <PrimaryButton text={"Checkout"} />
+            </Flex>
+          </div>
+        </Flex>
+      </section>
+    </div>
+  );
 };
 
 const OrderSection = styled(Orders)``;
